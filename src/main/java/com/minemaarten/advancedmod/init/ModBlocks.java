a5fb1be9ff91abf43ceb230f64951d2c01589542
MineMaarten <loopycoaster_8@hotmail.com>
2014-12-16 14:08:39 +0100

    Added ModBlocks class.

---
+++ src/main/java/com/minemaarten/advancedmod/init/ModBlocks.java
@@ -0,0 +1,13 @@
+package com.minemaarten.advancedmod.init;
+
+import com.minemaarten.advancedmod.reference.Reference;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
+public class ModBlocks{
+
+    public static void init(){
+
+    }
+}
