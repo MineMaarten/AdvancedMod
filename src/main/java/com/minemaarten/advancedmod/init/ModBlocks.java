f43c01ee444ebc3be794499278ec572d811af4a6
MineMaarten <loopycoaster_8@hotmail.com>
2014-12-16 14:40:46 +0100

    Added logging in the ModBlocks file. Fixes #1.

--- src/main/java/com/minemaarten/advancedmod/init/ModBlocks.java
+++ src/main/java/com/minemaarten/advancedmod/init/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.minemaarten.advancedmod.init;
 
 import com.minemaarten.advancedmod.reference.Reference;
+import com.minemaarten.advancedmod.utility.Log;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -8,6 +9,6 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModBlocks{
 
     public static void init(){
-
+        Log.info("Modblocks initialized");
     }
 }
