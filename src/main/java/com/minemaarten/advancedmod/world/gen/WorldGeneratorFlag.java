662adb756172bc5f48ab90d7b1c3de7bae13cc35
MSourceCoded <sourcecodedcreations@gmail.com>
2014-12-23 09:55:54 +0800

    Liquids pls

--- src/main/java/com/minemaarten/advancedmod/world/gen/WorldGeneratorFlag.java
+++ src/main/java/com/minemaarten/advancedmod/world/gen/WorldGeneratorFlag.java
@@ -3,6 +3,7 @@ package com.minemaarten.advancedmod.world.gen;
 import java.util.Random;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
@@ -50,7 +51,7 @@ public class WorldGeneratorFlag implements IWorldGenerator{
             int randY = world.getHeightValue(randX, randZ);
             if(world.getBiomeGenForCoords(randX, randZ) != BiomeGenBase.river && world.getBiomeGenForCoords(randX, randZ) != BiomeGenBase.ocean) {
                 Block block = world.getBlock(randX, randY - 1, randZ);
-                if(block != Blocks.water && block != Blocks.lava && block != Blocks.flowing_water && block != Blocks.flowing_lava) {
+                if (!(block instanceof BlockLiquid)) {
                     generateFlag(world, randX, randY, randZ);
                 }
             }
